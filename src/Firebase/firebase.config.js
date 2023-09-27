ffce52e852959249be8f7d77ebad7e65fdc66615
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-28 03:36:59 +0600

    Implement Simple Firebase Google Auth Integration

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAp3QFHHx0u_vlxeONnW_9DjLcwd5huxT4',
+  authDomain: 'dimple-firebase-eb1f7.firebaseapp.com',
+  projectId: 'dimple-firebase-eb1f7',
+  storageBucket: 'dimple-firebase-eb1f7.appspot.com',
+  messagingSenderId: '1027727442585',
+  appId: '1:1027727442585:web:835bfeff92d1f29cf42e08',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
